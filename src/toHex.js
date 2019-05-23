9aaae403c37adef9e5c5747092482bcae0b82761
David Binns <dwbinnspublic+git@gmail.com>
2019-05-23 22:06:30 +0100

    Changes

--- src/toHex.js
+++ src/toHex.js
@@ -1,3 +1,4 @@
 module.exports = function toHex(uint8array, lineFeed='') {
-    return `[${uint8array.length}]`+[...uint8array].map(v=>('00'+v.toString(16)).slice(-2)).join(':').match(/(..:?){1,16}/g).join(lineFeed);
+    //return uint8array.toString("hex");
+    return `[${uint8array.length}]`+[...uint8array].map(v=>('00'+v.toString(16)).slice(-2)).join(':')/*.match(/(..:?){1,16}/g).join(lineFeed)*/;
 }
